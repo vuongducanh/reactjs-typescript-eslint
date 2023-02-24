db363fbf67b5ec5b1ce10c2a14779e917f595123
vuongducanh <anhvuong730@gmail.com>
2023-02-24 16:25:25 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+interface TypeTest {
+  name: string;
+  id: number;
+}
+
+function App() {
+  const [active, setActive] = useState<boolean>(false);
+  const [value, setValue] = useState<TypeTest>({ name: 'aaa', id: 123 });
+
+  const setActiveItem = () => {
+    setActive(true);
+  };
+
+  useEffect(() => {
+    console.log(value);
+
+    setValue({
+      name: 'nbb',
+      id: 455,
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <div>{active ? 'active' : 'inactive'}</div>
+        <button onClick={setActiveItem}>Click</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;

2e1055cbc1fd81eeac5930641aeb67234e4e7bb3
vuongducanh <anhvuong730@gmail.com>
2023-02-24 16:30:58 +0700

    test lint

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ function App() {
   const [active, setActive] = useState<boolean>(false);
   const [value, setValue] = useState<TypeTest>({ name: 'aaa', id: 123 })
 
+
   const setActiveItem = () => {
     setActive(true);
   };
